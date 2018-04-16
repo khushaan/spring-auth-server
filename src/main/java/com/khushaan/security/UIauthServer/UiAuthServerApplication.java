594e5d6865b7c8d6913c5b5afde5023a882b8f11
unknown <soni.khushaan@gmail.com>
2018-04-16 17:54:27 -0400

    first commit

---
+++ src/main/java/com/khushaan/security/UIauthServer/UiAuthServerApplication.java
@@ -0,0 +1,12 @@
+package com.khushaan.security.UIauthServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UiAuthServerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/khushaan/security/UIauthServer/UiAuthServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.khushaan.security.UIauthServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UiAuthServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
